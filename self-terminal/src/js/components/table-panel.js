2b2910a5362d23e56550831a6af744f0d14b3c76
wangbin3162 <33084011+wangbin3162@users.noreply.github.com>
2019-08-30 16:19:35 +0800

    更新bin-ui为2.1.0，红黑名单列表查询页面

--- self-terminal/src/js/components/table-panel.js
+++ self-terminal/src/js/components/table-panel.js
@@ -1,7 +1,7 @@
 Vue.component('v-table-panel', {
   template: `
     <div class="table-panel">
-      <h2>{{label}}</h2>
+      <h2 v-if="label.length>0">{{label}}</h2>
       <slot></slot>
       <div class="back" v-if="!noBack">
         <b-button @click="$emit('on-back')">返回</b-button>
